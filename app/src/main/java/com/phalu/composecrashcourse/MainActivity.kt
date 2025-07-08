e3e473444ed7656e24364711f5e8b5cff4a79efe
PhalgunaRao <phalu900@gmail.com>
2025-07-08 17:27:41 +0530

    Added button and preview

--- app/src/main/java/com/phalu/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/phalu/composecrashcourse/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.phalu.composecrashcourse
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,32 +18,27 @@ import com.phalu.composecrashcourse.ui.theme.ComposeCrashCourseTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            ComposeCrashCourseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(){
+    Button(onClick = { Log.i("MainScreen","Some one Clicken me")}) {
+        Text(text = "Click me 1")
+    }
+    Button(onClick = { Log.i("MainScreen","Some one Clicken me")}) {
+        Text(text = "Click me 2")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeCrashCourseTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview(){
+    MainScreen()
 }
+
