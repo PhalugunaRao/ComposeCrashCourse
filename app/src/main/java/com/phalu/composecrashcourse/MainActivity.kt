af90d7af35822c9b4bc4f01d8acce7111caec002
PhalgunaRao <phalu900@gmail.com>
2025-07-10 15:33:12 +0530

    basic ui with surface

--- app/src/main/java/com/phalu/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/phalu/composecrashcourse/MainActivity.kt
@@ -6,66 +6,62 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.phalu.composecrashcourse.ui.theme.ComposeCrashCourseTheme
 
 class MainActivity : ComponentActivity() {
-    private val mainActivityViewModel by viewModels<MainActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            MainScreen(mainActivityViewModel)
-
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun MainScreen(mainActivityViewModel: MainActivityViewModel?){
-
-    mainActivityViewModel?.counter?.observeAsState()?.value
-    mainActivityViewModel?.error?.observeAsState()?.value.let {
-        Toast.makeText(LocalContext.current,it.toString(),Toast.LENGTH_SHORT).show()
-    }
-
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Row(verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth().padding(2.dp)
-            
-        ) {
-            Button(onClick = {mainActivityViewModel?.decrementCounter?.invoke() }, modifier = Modifier.padding(5.dp)) {
-                Text(text = "Decrement")
-            }
-            Text(text = "${mainActivityViewModel?.counter?.value}")
-            Button(onClick = {mainActivityViewModel?.incrementCounter?.invoke() }, modifier = Modifier.padding(5.dp)) {
-                Text(text = "Increment")
-            }
+fun MainScreen(){
+    ComposeCrashCourseTheme {
+        Surface(modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.surface) {
+            CountryCard()
         }
     }
+}
 
+@Composable
+fun CountryCard(){
+    Surface(modifier = Modifier
+        .fillMaxWidth(1.0f)
+        .padding(10.dp)
+        .wrapContentHeight(align = Alignment.Top)
+        .border(1.dp, Color.LightGray)) {
 
+        Text(text = "Hello World")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview(){
-    MainScreen(null)
+    MainScreen()
 }
 
