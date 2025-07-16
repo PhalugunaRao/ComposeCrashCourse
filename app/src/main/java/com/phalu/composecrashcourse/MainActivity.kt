6bdd8ece590f38ffd016becd74ee807f9212ab50
PhalgunaRao <phalu900@gmail.com>
2025-07-16 11:21:05 +0530

    Modifier order

--- app/src/main/java/com/phalu/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/phalu/composecrashcourse/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.phalu.composecrashcourse
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,8 +45,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen(indiaInfo)
+            //MainScreen(indiaInfo)
+            Greeting("Demo")
         }
+    }
+}
+
+@Composable
+fun Greeting(name:String){
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "Hello Phalu $name",
+            modifier = Modifier
+                .width(200.dp)
+                .height(100.dp)
+                .clip(RoundedCornerShape(2.dp))
+                .background(Color.LightGray)
+                .border(1.dp,Color.Black)
+                .clickable { Log.i("onclick","demo click") }
+                .padding(10.dp)
+
+        )
     }
 }
 
